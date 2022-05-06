77e124aae9ae36126e255b252c72afd378e273dc
otarampinelli <otaviorampinellielias@gmail.com>
2022-05-06 17:28:32 -0300

    feat: created tests to verify function operation to some possibilities

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ test("should insert two arrays and return false if are different", () => {
   expect(arrayIsEqual([true, 1, 'name', 5, 2, 1], [true, 1, false, 5, 2, 1])).toEqual(false);
 });
 
-test.only("Should return an error if is not an array", () => {
+test("Should return an error if is not an array", () => {
   expect(() => {
     arrayIsEqual([1, 2, 3], 'arroz');
   }).toThrow('Is only accepts array as parameter');
